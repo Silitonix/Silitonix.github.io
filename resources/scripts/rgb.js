ef7a338a15afa64ea52aec818f41c67355a84123
Silitonix <silitonix@gmail.com>
2023-07-12 17:23:00 +0330

    develop

--- resources/scripts/rgb.js
+++ resources/scripts/rgb.js
@@ -1,5 +1,5 @@
 const speed = 0.05;
 (function rgb(now=0) {
-  document.documentElement.style.setProperty('--rgb', `hsl(${((now * speed) % 360).toFixed(0)},96%,59%)`);
+  document.documentElement.style.setProperty('--rgb', `hsl(${((now * speed) % 360).toFixed(0)},75%,35%)`);
   requestAnimationFrame(rgb);
 })();
